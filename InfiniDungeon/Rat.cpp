27a2830d86b747cf5a40c65731ef2c12a066988e
armedturret <rjwhitenh@outlook.com>
2017-11-08 19:34:25 -0500

    Changed Rat Lore

--- InfiniDungeon/Rat.cpp
+++ InfiniDungeon/Rat.cpp
@@ -18,7 +18,7 @@ void Rat::OnCreation(const std::vector<std::vector<int>>& map, std::vector<std::
 {
 	m_health = 10;
 
-	m_lore = "A nasty sewer creature that somehow grew to the size of a man.";
+	m_lore = "These bloated rodents have become lazy and lethargic from time spent in a dungeon with scores of heroes come to feed them.";
 	m_name = "Giant Rat";
 
 	m_tileSheet.init(DPE::ResourceManager::getTexture("Data/Textures/Themes/Basic/Enemies/" + m_name+".png"), glm::ivec2(4, 2));
