9b20cdc6df3b648fd08266d9fec9c4d5da662f4c
NegNebulous <ajbewley1@gmail.com>
2017-11-30 21:35:50 -0500

    Updated Attack thingy

--- InfiniDungeon/Damage.cpp
+++ InfiniDungeon/Damage.cpp
@@ -17,9 +17,9 @@ void Damage::damageDoop(Creature& player)
 	//Sudo code
 	//int ran = Random::randInt(0, deffenderDex);
 
-	//if (ran < (deffenderDex / 3)) {maff = attackerDmg - (deffenderDex / 8)}
-	//if ((deffenderDex / 3) < ran < ((deffenderDex / 3) * 2)) {maff = attackerDex}
-	//if (ran > ((deffenderDex / 3) * 2)) {maff = (attackerDmg + (deffenderDex / 8) )}
+	//if (ran < (deffenderDex / 3)) {maff = attackerDmg - (attackerDmg / 8)}
+	//if ((deffenderDex / 3) < ran < ((deffenderDex / 3) * 2)) {maff = attackerDmg}
+	//if (ran > ((deffenderDex / 3) * 2)) {maff = (attackerDmg + (attackerDmg / 8) )}
 
 	//z = maff - DeffenderDef
 	//if (z < 0) {z = 0};
