c1cc00d57306d95c803303670f2994b3c2764ee7
armedturret <rjwhitenh@outlook.com>
2017-12-15 11:23:41 -0500

    Made working console

--- InfiniDungeon/Console.cpp
+++ InfiniDungeon/Console.cpp
@@ -3,6 +3,8 @@
 #include <chrono>
 #include <future>
 #include <string>
+#include <sstream>
+#include <iterator>
 Console::Console()
 {
 }
@@ -14,6 +16,12 @@ Console::~Console()
 
 void Console::init()
 {
+	Command::create_map();
+
+	//make commands
+	Command::create(Console::help, CommandInfo("help", "Stop it. Get some help.", "help OR help <pagenum> OR help <command>"));
+	Command::create(Console::echo, CommandInfo("echo", "Prints text that follows.", "echo <words..>"));
+
 	m_shouldEndThread = false;
 	console = std::thread(ConsoleRun::run, std::ref(console), std::ref(m_shouldEndThread));
 	std::cout << "Console thread initialized; Id: " << console.get_id() << std::endl;;
@@ -29,6 +37,27 @@ void Console::destroy()
 	std::cout << "Console thread joined" << std::endl;
 }
 
+int Console::help(std::vector<std::string> args)
+{
+	std::cout << "My name is jeff" << std::endl;
+	return 0;
+}
+
+int Console::echo(std::vector<std::string> args)
+{
+	if (args.size() > 1) {
+		std::cout << "[echo]: ";
+		for (int i = 1; i < args.size(); i++)
+			std::cout << args[i] << " ";
+		std::cout << "\n";
+		return 0;
+	}
+	else {
+		return 1;
+	}
+	
+}
+
 std::string ConsoleRun::getLineFromCin()
 {
 	std::string line;
@@ -36,19 +65,41 @@ std::string ConsoleRun::getLineFromCin()
 	return line;
 }
 
+int ConsoleRun::processCommand(std::string input)
+{
+	std::istringstream iss(input);
+	std::vector<std::string> tokens{ std::istream_iterator<std::string>{iss},
+		std::istream_iterator<std::string>{} };
+	if (tokens.size() > 0) {
+		Command cmd = Command::get_map().find(tokens[0])->second;
+		if (cmd.m_handler(tokens) == 1) {
+			std::cout << cmd.cmdInfo.helphint << std::endl;
+		}
+	}
+	return 0;
+}
+
 int ConsoleRun::run(std::thread& consoleThread, bool& shouldEndThread)
 {
 	auto future = std::async(std::launch::async, getLineFromCin);
-
+	
 	while (!shouldEndThread) {
 		if (future.wait_for(std::chrono::seconds(0)) == std::future_status::ready) {
 			auto line = future.get();
 
 			future = std::async(std::launch::async, getLineFromCin);
 
-			//do command processing here
+			processCommand(line);
 		}
 	}
 	consoleThread.detach();
 	return 0;
+}
+
+void Command::create(int(&handler)(std::vector<std::string>args), const CommandInfo & infoObject)
+{
+	Command tmpCommand;
+	tmpCommand.m_handler = &handler;
+	tmpCommand.cmdInfo = infoObject;
+	Command::get_map().insert(std::make_pair(tmpCommand.cmdInfo.name,tmpCommand));
 }
--- InfiniDungeon/Console.h
+++ InfiniDungeon/Console.h
@@ -1,9 +1,13 @@
 #pragma once
 #include <thread>
 #include <functional>
+#include <vector>
+#include <unordered_map>
 
 class Console
 {
+	
+	friend class ConsoleRun;
 public:
 	Console();
 	~Console();
@@ -22,6 +26,52 @@ private:
 	//console thread
 	std::thread console;
 
+	//return codes
+	// 0 = fine
+	// 1 = incorrect syntax
+	// 2 = custom syntax error
+	// 3 = internal error
+	//cmds
+	static int help(std::vector<std::string> args);
+	static int echo(std::vector<std::string> args);
+};
+
+struct CommandInfo {
+	CommandInfo() : name(""), desc(""), helphint("") {}
+	CommandInfo(std::string name, std::string desc, std::string helphint) : name(name), desc(desc), helphint(helphint) {}
+
+	//sorta like sv_cheats
+	std::string name;
+	//using 'help'
+	std::string desc;
+	//incorrect syntax
+	std::string helphint;
+};
+
+class Command
+{
+	friend class Console;
+	friend class ConsoleRun;
+public:
+	static std::unordered_map<std::string, Command> & create_map()
+	{
+		static std::unordered_map<std::string, Command> mymap;
+		/// init your map here
+		return mymap;
+	}
+
+	static std::unordered_map<std::string, Command> & get_map()
+	{
+		static std::unordered_map<std::string, Command> & mymap = create_map(); // this is only called once!
+		return mymap; // subsequent calls just return the single instance.
+	}
+
+	static void create(int(&handler) (std::vector<std::string> args), const CommandInfo& infoObject);
+
+private:
+	int(*m_handler) (std::vector<std::string> args);
+	CommandInfo cmdInfo;
+	static int currentIndex;
 };
 
 //seperate class for runtime
@@ -32,5 +82,6 @@ friend Console;
 private:
 	static std::string getLineFromCin();
 	//console function
+	static int processCommand(std::string input);
 	static int run(std::thread& consoleThread, bool& shouldEndThread);
 };
